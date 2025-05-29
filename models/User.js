da2e2646f098a96cf8625dafba48ee92b93bb2c9
techfusion <techfusion705@gmail.com>
2025-05-29 14:55:19 +0530

    forgot password implementation

--- models/User.js
+++ models/User.js
@@ -4,5 +4,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   password: String,
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 module.exports = mongoose.model('User', userSchema);
